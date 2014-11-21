68311c42ded54cfbad3671e97d0f5e4cc24e8d91
Sean James <seanjames777@gmail.com>
2014-11-21 18:08:53 -0500

    Document symbol table

--- include/statics/symboltable.h
+++ include/statics/symboltable.h
@@ -1,9 +1,7 @@
 /**
  * @file symboltable.h
  *
- * @brief Mapping from symbols to arbitrary information, usually types. This is
- * used for both variable types in a function body, and function and other types
- * at the top level.
+ * @brief Symbol tables
  *
  * @author Sean James <seanjames777@gmail.com>
  */
@@ -13,9 +11,13 @@
 
 #include <defs.h>
 
-// TODO: It is not clear that this is more useful than a simple map
-// TODO: Might want this to be a utility
-
+/**
+ * @brief A map from symbols to arbitrary information, usually types. This is
+ * used for both variable types in a function body, and function and other types
+ * at the top level.
+ *
+ * @tparam T Type to map symbols to
+ */
 template<typename T>
 class SymbolTable {
 private:
@@ -24,21 +26,47 @@ private:
 
 public:
 
+    /**
+     * @brief Constructor
+     */
     SymbolTable() {
     }
 
+    /**
+     * @brief Set the value a symbol maps to
+     *
+     * @param[in] symbol Symbol to map from
+     * @param[in] val    Symbol to map to
+     */
     void set(std::string symbol, T val) {
         table[symbol] = val;
     }
 
+    /**
+     * @brief Get the value a symbol is mapped to, if it exists
+     *
+     * @param[in] symbol Symbol mapped from
+     *
+     * @return Value mapped to
+     */
     T get(std::string symbol) {
         return table[symbol];
     }
 
+    /**
+     * @brief Check if the symbol table contains a mapping for a symbol
+     *
+     * @param[in] symbol Symbol mapped from
+     *
+     * @return Whether there is a mapping
+     */
     bool hasSymbol(std::string symbol) {
         return table.find(symbol) != table.end();
     }
 
+    /**
+     * @brief Remove all entries from the symbol table
+     */
     void clear() {
         table.clear();
     }
