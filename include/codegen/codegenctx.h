786af777394bbf53a20eb5b55a6e730f63ebc6c0
Sean James <seanjames777@gmail.com>
2014-11-23 03:19:45 -0500

    Move LLVM headers

--- include/codegen/codegenctx.h
+++ include/codegen/codegenctx.h
@@ -14,6 +14,24 @@
 #include <statics/functioninfo.h>
 #include <statics/moduleinfo.h>
 
+#include <llvm/IR/Module.h>
+#include <llvm/IR/LLVMContext.h>
+#include <llvm/IR/Value.h>
+#include <llvm/IR/Constants.h>
+#include <llvm/IR/Type.h>
+#include <llvm/IR/Function.h>
+#include <llvm/IR/DerivedTypes.h>
+#include <llvm/IR/Instructions.h>
+#include <llvm/PassManager.h>
+#include <llvm/Support/raw_ostream.h>
+#include <llvm/IR/IRPrintingPasses.h>
+#include <llvm/ADT/Triple.h>
+#include <llvm/Support/raw_os_ostream.h>
+#include <llvm/IR/IRBuilder.h>
+#include <llvm/Transforms/Scalar.h>
+
+using namespace llvm;
+
 namespace Codegen {
 
 class CodegenCtx {
--- include/codegen/converttype.h
+++ include/codegen/converttype.h
@@ -11,6 +11,25 @@
 
 #include <ast/type/asttypenode.h>
 
+// TODO
+#include <llvm/IR/Module.h>
+#include <llvm/IR/LLVMContext.h>
+#include <llvm/IR/Value.h>
+#include <llvm/IR/Constants.h>
+#include <llvm/IR/Type.h>
+#include <llvm/IR/Function.h>
+#include <llvm/IR/DerivedTypes.h>
+#include <llvm/IR/Instructions.h>
+#include <llvm/PassManager.h>
+#include <llvm/Support/raw_ostream.h>
+#include <llvm/IR/IRPrintingPasses.h>
+#include <llvm/ADT/Triple.h>
+#include <llvm/Support/raw_os_ostream.h>
+#include <llvm/IR/IRBuilder.h>
+#include <llvm/Transforms/Scalar.h>
+
+using namespace llvm;
+
 namespace Codegen {
 
 Type *convertType(std::shared_ptr<ASTTypeNode> type);
