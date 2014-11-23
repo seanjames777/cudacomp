1af2d153f9129ebeab4c8c1d13f8fe36760f771b
Sean James <seanjames777@gmail.com>
2014-11-23 02:24:34 -0500

    Fix bug with functions that are called but never defined

--- include/ast/astvisitor.h
+++ include/ast/astvisitor.h
@@ -9,33 +9,7 @@
 #ifndef __ASTVISITOR_H
 #define __ASTVISITOR_H
 
-#include <ast/astnode.h>
-#include <ast/astseqnode.h>
-#include <ast/decl/astdeclnode.h>
-#include <ast/decl/astfundecl.h>
-#include <ast/decl/asttypedecl.h>
-#include <ast/expr/astbinopexp.h>
-#include <ast/expr/astbooleanexp.h>
-#include <ast/expr/astcallexp.h>
-#include <ast/expr/astexpnode.h>
-#include <ast/expr/astidentifierexp.h>
-#include <ast/expr/astintegerexp.h>
-#include <ast/expr/astunopexp.h>
-#include <ast/stmt/astexprstmt.h>
-#include <ast/stmt/astifstmt.h>
-#include <ast/stmt/astreturnstmt.h>
-#include <ast/stmt/astscopestmt.h>
-#include <ast/stmt/aststmtnode.h>
-#include <ast/stmt/astvardeclstmt.h>
-#include <ast/stmt/astvardefnstmt.h>
-#include <ast/type/astargnode.h>
-#include <ast/type/astbooleantype.h>
-#include <ast/type/astfuntype.h>
-#include <ast/type/astidtype.h>
-#include <ast/type/astintegertype.h>
-#include <ast/type/astptrtype.h>
-#include <ast/type/asttypenode.h>
-#include <ast/type/astvoidtype.h>
+#include <ast/ast.h>
 
 #define VISIT_HDR(NAME, ARG) virtual bool visit##NAME(std::shared_ptr<AST##NAME> ARG);
 #define VISIT_IMPL(NAME, ARG) \
