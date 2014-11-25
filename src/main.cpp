65d34130f1fe40e1672de6d3a33a2a701f4ebd99
Sean James <seanjames777@gmail.com>
2014-11-25 14:51:12 -0500

    Add a help message

--- src/main.cpp
+++ src/main.cpp
@@ -19,11 +19,28 @@ struct CCArgs {
     char *out_file;
 } args;
 
+void printHelp(char *argv[]) {
+    printf(
+        "Usage: %s [-h] [--print-ast] [--emit-device] [-o <file>] [<file>]\n"
+        "\n"
+        "Options:\n"
+        "    -h            Print this help message and exit\n"
+        "    --print-ast   Print a description of the parsed abstract syntax tree\n"
+        "    --emit-device Emit the entire program as PTX code, if possible\n"
+        "    -o <file>     Output assembly to a file instead of standard out\n"
+        "    <file>        File to parse, instead of standard in\n"
+        , argv[0]);
+}
+
 void parseArgs(int argc, char *argv[]) {
     memset(&args, 0, sizeof(args));
 
     for (int i = 1; i < argc; i++) {
-        if (strcmp(argv[i], "--emit-device") == 0)
+        if (strcmp(argv[i], "-h") == 0 || strcmp(argv[i], "--help") == 0) {
+            printHelp(argv);
+            exit(0);
+        }
+        else if (strcmp(argv[i], "--emit-device") == 0)
             args.emit_device = true;
         else if (strcmp(argv[i], "--print-ast") == 0)
             args.print_ast = true;
